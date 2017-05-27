a50a05fb3f1fd9a3051ad6d1476a5e598d58774f
Mark Orel <mail.ormark@gmail.com>
2017-05-27 22:11:03 +0300

    added config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const isPord = process.env.END === 'production' ? true : false;
+console.log('PROD: ', isPord);
 const d = (dev, prod) => {
   if (isPord) {
     return prod;
@@ -7,7 +8,7 @@ const d = (dev, prod) => {
 }
 
 module.exports = {
-  host: d('localhost', '54.154.31.253'),
+  host: d('localhost', 'ec2-54-154-31-253.eu-west-1.compute.amazonaws.com'),
   socketPort: d(9999, 9999),
   port: d(800, 800),
   api: d(
